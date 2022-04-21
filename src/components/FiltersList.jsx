9d37fde4a4f977b1b64d38b957d743b9c5d54017
Rafael Godoy <rafa.qfg@gmail.com>
2022-04-21 17:52:01 -0300

    finaliza logica de eliminar filtros individuais

--- src/components/FiltersList.jsx
+++ src/components/FiltersList.jsx
@@ -1,22 +1,29 @@
 import React, { useContext } from 'react';
-// import PropTypes from 'prop-types';
 import StarWarsContext from '../context/starWarsContext';
 
 function FiltersList() {
-  const { filterByNumericValues } = useContext(StarWarsContext);
+  const { filterByNumericValues, setFilterByNumericValues } = useContext(StarWarsContext);
+
+  const deleteFilter = ({ target }) => {
+    const updateList = filterByNumericValues.filter((item) => target.id !== item.column);
+    setFilterByNumericValues(updateList);
+  };
+
   return (
     <ul>
       {
-        filterByNumericValues.map((filter, index) => (
+        filterByNumericValues.map((filter) => (
           <li
-            key={ [index] }
+            key={ [filter.column] }
             data-testid="filter"
           >
             <spam>{`${filter.column} `}</spam>
             <spam>{`${filter.operator} `}</spam>
             <spam>{`${filter.value} `}</spam>
             <button
+              id={ filter.column }
               type="button"
+              onClick={ deleteFilter }
             >
               x
             </button>
